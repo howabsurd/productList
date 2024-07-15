13630e483a8effa6a5e1cf61a6db413e5a059141
howabsurd <poptanikrish1@gmail.com>
2024-07-15 09:59:07 +0530

    installing cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import router from "./router/index"
+import cors from "cors";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use((req,res,next)=>{
     console.log(req.method);
     next()
 })
+app.use(cors());
 
 // Import Prisma Client
 import { PrismaClient } from '@prisma/client';
